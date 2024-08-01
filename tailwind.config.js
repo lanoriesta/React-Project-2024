eac278960d4839e0f726999ea3c484b2ed1d8fe3
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2024-08-01 22:32:28 +0800

    Update screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ export default {
         padding: "2rem",
       },
     },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
+    },
   },
   plugins: [],
 };
