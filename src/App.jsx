701d0b14926bed5eb2db067dcf4ea66a91d09f31
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2024-08-01 22:31:09 +0800

    Add LoadMore component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Accordion from "./components/Accordion";
 import ImageSlider from "./components/ImageSlider";
+import LoadMoreBtn from "./components/LoadMoreBtn";
 import RandomColorGenerator from "./components/RandomColorGenerator";
 import StarRating from "./components/StarRating";
 
@@ -16,7 +17,10 @@ function App() {
       {/* <StarRating numberOfStar={10} /> */}
 
       {/* Image Slider */}
-      <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} />
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={10} /> */}
+
+      {/* Load More btn */}
+      <LoadMoreBtn />
     </>
   );
 }
