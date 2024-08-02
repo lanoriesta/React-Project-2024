ff67aff262ab3c4378dd7006f845d4b2b76facf4
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2024-08-03 05:45:33 +0800

    Add icons for TreeView component

---
+++ src/assets/plusIcon.jsx
@@ -0,0 +1,19 @@
+export function PlusIcon(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="#fff"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+        d="M12 6v12m6-6H6"
+      ></path>
+    </svg>
+  );
+}
